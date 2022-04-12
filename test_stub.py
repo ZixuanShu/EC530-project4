3ba816d84bf5e4f72bdd80eb3d0c354aeedc0e78
ZixuanShu <60206922+ZixuanShu@users.noreply.github.com>
2022-04-11 20:02:08 -0400

    readme and yaml update

--- test_stub.py
+++ test_stub.py
@@ -5,4 +5,4 @@ expected_output = 30
 test_input = [10,17,3,10,6,8,7,4,9]
 def test_stub():
     resulting_output = Queue_system_stub.stub_queue(test_input)
-    assert resulting_output < expected_output
+    assert int(resulting_output) < expected_output
